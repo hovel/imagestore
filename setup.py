93ff182e5e8658d7e1d73bea330d917572fa63d3
Pavel Zhukov <gelios@gmail.com>
2009-11-29 17:41:04 +0300

    version push

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name = 'imagestore',
-        version = '1.0',
+        version = '1.0.1',
             packages = ['imagestore'],
         install_requires = [
             'django',
