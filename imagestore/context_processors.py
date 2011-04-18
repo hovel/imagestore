4925096b3a977054f4cd79fb1360097de1dcd011
Zhukov Pavel <gelios@gmail.com>
2011-04-18 11:15:16 +0400

    Translation fixes, optional show user info

--- imagestore/context_processors.py
+++ imagestore/context_processors.py
@@ -6,8 +6,12 @@ from django.conf import settings
 
 def imagestore_processor(request):
     template = getattr(settings, 'IMAGESTORE_TEMPLATE', False)
+    imagestore_show_user = getattr(settings, 'IMAGESTORE_SHOW_USER', True)
     if template:
-        return {'IMAGESTORE_TEMPLATE': template}
+        return {
+            'IMAGESTORE_TEMPLATE': template,
+            'IMAGESTORE_SHOW_USER': imagestore_show_user,
+        }
     else:
         return {}
 
