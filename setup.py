6bf59138137926256b2e03f5d8cdd1734243dfba
Zhukov Pavel <gelios@gmail.com>
2011-06-13 13:03:58 +0400

    Include README to MANIFEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'imagestore',
-        version = '2.1.1',
+        version = '2.1.3',
         packages = find_packages(),
         install_requires = [
             'django',
