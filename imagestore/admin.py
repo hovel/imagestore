9c32600d456bd23ce9a94ee688ffe16cba4ab8ce
Zhukov Pavel <gelios@gmail.com>
2011-06-22 12:18:49 +0400

    Add inline image edit in admin view

--- imagestore/admin.py
+++ imagestore/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from imagestore.models import Image, Album, AlbumUpload
-from sorl.thumbnail.admin import AdminImageMixin
+from sorl.thumbnail.admin import AdminImageMixin, AdminInlineImageMixin
 
 try:
     from places.models import GeoPlace
 except:
     GeoPlace = None
 
+class InlineImageAdmin(AdminInlineImageMixin, admin.TabularInline):
+    model = Image
+    fieldsets = ((None, {'fields': ['image', 'user', 'title', 'order', 'tags', 'album']}),)
+    raw_id_fields = ('user', )
+    extra = 0
+
 class AlbumAdmin(admin.ModelAdmin):
     fieldsets = ((None, {'fields': ['name', 'user', 'is_public']}),)
     list_display = ('name', 'admin_thumbnail','user', 'created', 'updated', 'is_public')
+    inlines = [InlineImageAdmin]
 
 admin.site.register(Album, AlbumAdmin)
 
@@ -17,6 +24,7 @@ class ImageAdmin(admin.ModelAdmin):
     fieldsets = ((None, {'fields': ['user', 'title', 'image', 'description', 'order', 'tags', 'album']}),)
     list_display = ('admin_thumbnail', 'user', 'order', 'album', 'title')
     raw_id_fields = ('user', )
+    list_filter = ('album', )
 
 class AlbumUploadAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
