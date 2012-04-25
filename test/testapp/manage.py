f485720c2b0703b51e4c1910101e92864ecb4d52
Pavel Zhukov <gelios@gmail.com>
2012-04-25 15:12:10 +0400

    Add gitignore

--- test/testapp/manage.py
+++ test/testapp/manage.py
@@ -2,6 +2,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath('./../../'))
+sys.path.insert(0, os.path.abspath('./../'))
 
 from django.core.management import execute_manager
 import imp
