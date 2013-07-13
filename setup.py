8ea942b6a8917eef9ffa1bd1ce629598e848ad07
Sergey Fursov <GeyseR85@gmail.com>
2013-07-13 18:38:38 +0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'imagestore',
-        version = '2.7.3',
+        version = '2.7.4',
         packages = find_packages(),
         install_requires = [
             'django',
