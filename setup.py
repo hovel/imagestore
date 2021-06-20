d75e9d782d0bfdc3573724cfd4045050a9756bb4
lampslave <lampslave@gmail.com>
2021-06-20 15:49:19 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     version='3.2.0',
     packages=find_packages(),
     install_requires=[
-        'django>=1.11',
-        'pillow',
+        'django>=2.2',
+        'pillow>=5.4.1',
         'sorl-thumbnail>=12.4.0',
-        'django-autocomplete-light',
+        'django-autocomplete-light>=3.0',
         'django-tagging',
         'swapper',
         'cchardet>=2.1.4',
@@ -21,7 +21,7 @@ setup(
     author_email='gelios@gmail.com',
     description='Gallery solution for django projects',
     long_description=README,
-    license='GPL',
+    license='BSD 3-Clause',
     keywords='django gallery',
     url='https://github.com/hovel/imagestore',
     include_package_data=True
